5be8bd0878e8b599afe97b4aad2764e2ce101ffa
Dong Joo Kwon <dongjoo7@outlook.com>
2022-06-12 15:01:33 -0400

    Co-authored-by: annie551 <annie551@users.noreply.github.com> Co-authored-by: kaynam23 <kaynam23@users.noreply.github.com>

--- App.js
+++ App.js
@@ -1,25 +1,14 @@
 import { StatusBar } from 'expo-status-bar';
-import { Image,  AppRegistry, StyleSheet, TouchableOpacity, Alert, Text, View, Button } from 'react-native';
+import { Image, StyleSheet, TouchableOpacity, Text, View, Button, Alert } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
+
+
+import React, { Component } from 'react';
 import { Table, TableWrapper, Row, Cell } from 'react-native-table-component';
-import React, { Component, useEffect, useState} from 'react';
-import {StreamChat, OverlayProvider, Chat, ChannelList, MessageList} from 'stream-chat-react-native';
-import  {SafeAreaProvider} from 'react-native-safe-area-context';
-import App from './App';
-import { registerRootComponent } from 'expo';
 
-AppRegistry.registerComponent('main', () => App);
 
-// if (Platform.OS === 'ios') {
-//   const rootTag = document.getElementById('root') || document.getElementById('Main');
-//   AppRegistry.runApplication('Main', { rootTag });
-// }
-
-const API_KEY = "p7xv2xrch8yw";
-const client = StreamChat.getInstance(API_KEY);
-
-import bcabuslogo from './assets/bcabuslogo.png'; 
+import bcabuslogo from './assets/bcabuslogo.png';
 // import { AwesomeButton } from "react-awesome-button";
 // import AwesomeButtonStyles from "react-awesome-button/src/styles/styles.scss";
 
@@ -29,7 +18,7 @@ import styles from "./style"
 function HomeScreen({ navigation }) {
   return (
     <View style={styles.container}>
-      <Image source={bcabuslogo} style={styles.logo} /> 
+      <Image source={bcabuslogo} style={styles.logo} />
       <Text style={styles.title} >Bus Buddies!</Text>
       <Button
         title="SendNotifScreen"
@@ -42,8 +31,6 @@ function HomeScreen({ navigation }) {
   );
 }
 
-// HELLO
-
 
 function SecondScreen() {
   return (
@@ -54,94 +41,88 @@ function SecondScreen() {
 }
 
 
-function SendNotifScreen() {
-  let r = RouteTable()
+function SendNotifScreen1() {
   return (
     <View style={styles.container}>
       <Button
         title="Sending Bus Notif"
-        onPress={result}
+      //onPress={result}
       />
     </View>
   )
 }
 
-const Stack = createNativeStackNavigator();
-
-export default function Main() {
-  const [isReady, setIsReady] = useState(false);
-  const [selectedChannel, setSelectedChannel] = useState(true);
-
-  useEffect(() => {
-    const connectUser = async () => {
-      await client.connectUser (
-        {
-            id: 'christina',
-            name: 'Christina Kaddouh',
-        },
-        client.devToken('christina')
-      );
-      console.log("User connected");
-
-      const channel = client.channel("messaging", "busbuddies", {
-        name: "River Vale/Hillsdale",
-      });
-      await channel.watch();
-
-      setIsReady(true);
-    };
-
-    connectUser();
-
-    return () => client.disconnectUser();
-  }, [])
-
-   
-
-    const onChannelPressed = (channel) => {
-      setSelectedChannel(channel);
+class SendNotifScreen extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      tableHead: ['Head', 'Head2', 'Head3', 'Head4'],
+      tableData: [
+        ['1', '2', 'passed', '4'],
+        ['a', 'b', 'c', 'd'],
+        ['1', '2', '3', '4'],
+        ['a', 'b', 'c', 'd']
+      ]
     }
+  }
 
-    console.log(isReady);
-    if (!isReady) {
-      return null;
-    }
-    else {
-      
-      return (
-        <SafeAreaProvider>
-        <OverlayProvider>
-          {/* <Chat client = {client}>
+  _alertIndex(index) {
+    Alert.alert(`This is row ${index + 1}`);
+  }
 
-            {selectedChannel ? (
-              <Channel channel={selectedChannel}>
-                <MessageList />
-                <MessageInput />
-                <Text style={{margin: 40}}
-                      onPress={() => setSelectedChannel(null)}>Go back</Text>
-              </Channel>
-            ) : (
-            <ChannelList onSelect={onChannelPressed}/>
-            )}
-          </Chat> */}
-        </OverlayProvider>
-        <StatusBar />
-        </SafeAreaProvider>
-      );
+  render() {
+    const state = this.state;
+    const element = (data, index) => (
+      <TouchableOpacity onPress={() => this._alertIndex(index)}>
+        <View style={styles1.btn}>
+          <Text style={styles1.btnText}>button</Text>
+        </View>
+      </TouchableOpacity>
+    );
 
-      // return (
-      //   <NavigationContainer>
-      //     <Stack.Navigator initialRouteName="Home">
-      //       <Stack.Screen name="HomeScreen" component={HomeScreen} />
-      //       <Stack.Screen name="SendNotifScreen" component={SendNotifScreen} />
-      //     </Stack.Navigator>
-      //   </NavigationContainer>
-      // );
-    }
-
-    
-    
+    return (
+      <View style={styles1.container}>
+        <Table borderStyle={{ borderColor: 'transparent' }}>
+          <Row data={state.tableHead} style={styles1.head} textStyle={styles1.text} />
+          {
+            state.tableData.map((rowData, index) => (
+              <TableWrapper key={index} style={styles1.row}>
+                {
+                  rowData.map((cellData, cellIndex) => (
+                    <Cell key={cellIndex} data={cellIndex === 3 ? element(cellData, index) : cellData} textStyle={styles.text} />
+                  ))
+                }
+              </TableWrapper>
+            ))
+          }
+        </Table>
+      </View>
+    )
+  }
 }
 
+const styles1 = StyleSheet.create({
+  container: { flex: 1, padding: 16, paddingTop: 30, backgroundColor: '#125e50' },
+  head: { height: 40, backgroundColor: '#FFC300' },
+  text: { margin: 6 },
+  row: { flexDirection: 'row', backgroundColor: '#FFF1C1' },
+  btn: { width: 58, height: 18, backgroundColor: '#78B7BB', borderRadius: 2 },
+  btnText: { textAlign: 'center', color: '#fff' }
+});
 
-// export default App;
+
+
+const Stack = createNativeStackNavigator();
+
+function App() {
+  return (
+    <NavigationContainer>
+      <Stack.Navigator initialRouteName="Home">
+        <Stack.Screen name="HomeScreen" component={HomeScreen} />
+        <Stack.Screen name="SendNotifScreen" component={SendNotifScreen} />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
+}
+
+export default App;
